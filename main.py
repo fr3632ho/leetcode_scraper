0b5c7a2387a30edf177010bd40e0d8f9a84c9954
fr3632ho <fr3632ho-s@student.lu.se>
2022-10-28 12:38:09 +0800

    added main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json, requests, time, re
-from sys import api_version
-import bs4
+import json, requests, time, re, textwrap, pdfkit
 
+from PyPDF2 import PdfFileMerger
+from collections import defaultdict
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -32,6 +32,8 @@ stat_keys = [PROBLEM_TITLE_SLUG_KEY, \
 DRIVER_PATH = r'/usr/bin/safaridriver'
 JSON_DATA_PATH = r'./assets/leetcode_data.json'
 JSON_WANTED_PATH = r'./assets/wanted_problems.json'
+HTML_TEST_PATH = r'./assets/test.html'
+PDF_OUT_PATH = r'./assets/pdfs/pdf_'
 
 # start a safari window
 driver = webdriver.Safari(executable_path=DRIVER_PATH)
@@ -39,13 +41,12 @@ driver = webdriver.Safari(executable_path=DRIVER_PATH)
 def create_problem_dictionary(problem_data) -> dict:
     # need diff, title, title_slug, total_acs, total_sub, question_id
     stat = problem_data[PROBLEM_STAT_KEY]
-    obj = {k:stat[k] for k in stat}
+    obj = {k:stat[k] for k in stat if k in stat_keys}
     obj[PROBLEM_DIFFICULTY_KEY] = problem_data[PROBLEM_DIFFICULTY_KEY]
     return obj
 
 def select_all_button():
     driver.find_element(by = By.PARTIAL_LINK_TEXT, value = 'NeetCode All').click()
-
 
 '''
 Fetch the wanted problems from neetcode.io and 
@@ -56,27 +57,28 @@ def fetch_wanted_problems() -> dict:
     driver.get(NEETCODE_ENDPOINT)    
     time.sleep(2)
     select_all_button()
-
-    # containers = driver.find_elements(by = By.CLASS_NAME, value = 'ng-star-inserted')        
-    # for elem in containers:    
-    #     names = elem.find_elements(by = By.CLASS_NAME, value=r'ng-tns-c25-55')                
-    #     print(names)
-
-    # return dict()        
-    hrefs = driver.find_elements(by = By.XPATH,value="//a[@href]")
-    filtered_problems = []
-    pattern = re.compile('(\/*[0-9a-z\-]*\/?)$')          
-    for element in hrefs:
-        reference = element.get_attribute('href')
-        if reference.startswith(LEETCODE_ENDPOINT):  
-            problem_name = pattern.search(reference).group(1)[1:-1]            
-            filtered_problems.append(problem_name)  
-    
-    # Write to file
+    time.sleep(1)
+    containers = driver.find_elements(by = By.CSS_SELECTOR, value = 'app-pattern-table.ng-star-inserted')        
     wanted = dict()
-    wanted['problems'] = filtered_problems
+    for elem in containers:    
+        button = elem.find_element(by = By.TAG_NAME, value="button")        
+       
+        hrefs = elem.find_elements(by = By.XPATH, value = ".//a[@href]")
+        filtered_problems = []
+        pattern = re.compile('(\/*[0-9a-z\-]*\/?)$')           
+        for element in hrefs:
+            reference = element.get_attribute('href')            
+            if reference.startswith(LEETCODE_ENDPOINT):  
+                problem_name = pattern.search(reference).group(1)[1:-1]            
+                filtered_problems.append(problem_name)  
+
+        pattern = re.compile('(^[^(]*)')
+        key = pattern.search(button.text).group(1)
+        wanted[key] = filtered_problems
+             
+    # Write to file    
     with open(JSON_WANTED_PATH, 'w', encoding='utf-8') as file:    
-        print("Dumping {} Leetcode problems found on neetcode.io to file".format(len(filtered_problems)), file.name)    
+        print("Dumping {} Leetcode problems found on neetcode.io to file".format(305), file.name)    
         json.dump(wanted, file, ensure_ascii=False, indent=4)    
     return wanted
 
@@ -89,19 +91,75 @@ def fetch_leetcode_data() -> dict:
         print("Dumped API contents into",file.name)
     return data_json
 
-def main():           
-    wanted = set(fetch_wanted_problems()['problems'])    
-    data_json = fetch_leetcode_data()
+def generate_header(item) -> str:
+    level = item[PROBLEM_DIFFICULTY_KEY]['level']
+    if level == 3:
+        level = '<p style="color: red"> Hard </p>'
+    elif level == 2:
+        level = '<p style="color: orange"> Medium </p>'
+    else:
+        level = '<p style="color: green"> Easy </p>'
+    problem_title = '<head> <strong> {} </strong> {}  </head>\n'.format(item[PROBLEM_TITLE_KEY], level)                  
+    return problem_title
 
-    filtered_problems = []
+'''
+Return a file path to the created pdf
+'''
+def download_single_leetcode_html(item : dict) -> str:
+    driver.get(LEETCODE_ENDPOINT + item[PROBLEM_TITLE_SLUG_KEY])        
+    element = driver.find_element(by = By.CSS_SELECTOR, value = "div.description__24sA")
+    element = element.find_element(by = By.XPATH, value = ".//div[contains(@class, 'content__u3I1 question-content__JfgR')]")        
+    
+    text = textwrap.fill(element.get_attribute('innerHTML'), 80)
+    # insert title of problem and difficulty
+    problem_title = generate_header(item)
+    match_end = re.search('<div>', text).span()[1]                
+    text = text[:match_end] + problem_title + text[match_end:]
+
+    with open(HTML_TEST_PATH, 'w') as file:
+        file.write(text)            
+        file.close()
+
+    pdfkit.from_file(HTML_TEST_PATH, PDF_OUT_PATH)
+
+    return text
+     
+
+def load_neetcode_data() -> dict:
+    with open(JSON_WANTED_PATH, 'r') as file:
+        return json.load(file)
+
+def load_leetcode_data() -> dict:
+    with open(JSON_DATA_PATH, 'r') as file:
+        return json.load(file)    
+
+
+def create_group_pdf(group_name : str, problems : list) -> str:
+    # first create a title for the grouped pdf
+    # loop through all problems and create a pdf for each and every problem
+    # merge all pdfs into one 
+    # return the path to said pdf
+    return None
+
+def create_pdf(problems : dict) -> None:
+
+    for group_name, data in problems.items():
+        create_group_pdf(group_name, data)
+
+def main():           
+    wanted = load_neetcode_data()    
+    wanted_problems = {item:k for k,sublist in wanted.items() for item in sublist}    
+    data_json = load_leetcode_data()
+    
+    filtered_problems = defaultdict(list)
     for problem_data in data_json[PROBLEM_KEY]:
         # Need to filter the data to only take problems that we want
-        # Done by filtering on 'question__title_slug in wanted'        
-        if problem_data[PROBLEM_STAT_KEY][PROBLEM_TITLE_SLUG_KEY] in wanted:
-            filtered_problems.append(create_problem_dictionary(problem_data))
-    
-    # TODO we need to download the description of each problem to one page
-    # 
+        # Done by filtering on 'question__title_slug in wanted'    
+        key = problem_data[PROBLEM_STAT_KEY][PROBLEM_TITLE_SLUG_KEY]
+        if key in wanted_problems.keys():
+            filtered_problems[key].append(create_problem_dictionary(problem_data))        
+
+    create_pdf(filtered_problems)    
 
 if __name__ == "__main__":
     try:
